7c4e8fd7f93b212537a5b066ba3e032de142bafd
lchsk <mjlechowski@gmail.com>
2018-08-26 15:55:10 +0100

    Fmt main.go

--- main.go
+++ main.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-    "bufio"
-    "encoding/csv"
-    "strconv"
-    "fmt"
-    "io"
-    "log"
-    "os"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
 )
 
 type Song struct {
-    Album string
-    AlbumYear  int
-    Name   string
-    Text   string
+	Album     string
+	AlbumYear int
+	Name      string
+	Text      string
 }
 
 func main() {
-    csvFile, _ := os.Open("data/all_songs_with_metadata.csv")
+	csvFile, _ := os.Open("data/all_songs_with_metadata.csv")
 
-    reader := csv.NewReader(bufio.NewReader(csvFile))
+	reader := csv.NewReader(bufio.NewReader(csvFile))
 
-    var songs []Song
+	var songs []Song
 
-    for {
-        line, error := reader.Read()
+	for {
+		line, error := reader.Read()
 
-        if error == io.EOF {
-            break
-        } else if error != nil {
-            log.Fatal(error)
-        }
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
 
-        year, err := strconv.Atoi(line[1])
+		year, err := strconv.Atoi(line[1])
 
-        if err != nil {
-            year = 0
-        }
+		if err != nil {
+			year = 0
+		}
 
-        songs = append(songs, Song{
-            Album: line[0],
-            AlbumYear:  year,
-            Name:  line[2],
-            Text: line[3],
-        })
-    }
-    fmt.Println(songs[0].Text)
-    fmt.Println(len(songs))
+		songs = append(songs, Song{
+			Album:     line[0],
+			AlbumYear: year,
+			Name:      line[2],
+			Text:      line[3],
+		})
+	}
+	fmt.Println(songs[0].Text)
+	fmt.Println(len(songs))
 }
